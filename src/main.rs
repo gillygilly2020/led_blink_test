85e04d48a48331c4b4ea4684ea71693c5fc41377
gillygilly2020 <lg1086@rit.edu>
2025-09-25 17:44:56 +0200

    push

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::{thread::sleep, time::Duration};
+
+use rppal::gpio::Gpio;
+
+fn main() {
+   let gpio = Gpio::new().expect("error reaching GPIO");
+   let mut pin = gpio.get(17)?.into_output();
+   for i in 0..=10{
+    pin.set_high();
+    sleep(Duration::from_millis(500));
+    pin.set_low();
+    sleep(Duration::from_millis(500));
+   }
+}
